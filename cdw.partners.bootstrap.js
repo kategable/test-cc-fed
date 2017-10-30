58a56baef2b7680fcb58148f545d99ba77c3d01f
Kate Sky <katesky@cdw.com>
2017-10-30 08:39:54 -0500

    update of code

--- cdw.partners.bootstrap.js
+++ cdw.partners.bootstrap.js
@@ -1,53 +1,56 @@
-
+"use strict";
 (function cdwloader() {
 
-    var bootstrapcss = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css";
-    var testServer = "https://lt-a0062163.corp.cdw.com/payments/_authorize";
-    var liveServer = "https://somepartnersapiserverurl";//TODO: change to live server
 
-    var api = new ApiModule(bootstrapcss, testServer, liveServer);
+    // var testServer = "https://localhost:44390";
+    var testServer = "http://localhost:54895";
+    var liveServer = "https://somepartnersapiserverurl";//TODO: change to live server
+    var action = "/payments/_authorize";
+    var api = new ApiModule(testServer, liveServer, action);
 
     var main = function () {
 
         api.loadForm();
     }
 
+
     if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.1.1') {
         api.loadScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', function () {
-            api.jQuery = window.jQuery;
-            api.loadScript('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js', function () {
-                api.jQuery = window.jQuery;
-                api.loadScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js', main);
-            });
+            api.setjQuery(window.jQuery);
+            main();
         });
     } else {
-        api.loadScript('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js', main);
+        api.setjQuery(window.jQuery);
     }
-
 })();
 
-function ApiModule(bootstrapcss, test, live) {
-    var formId = '';
+function ApiModule(test, live, action) {
+    var formId;
     var btnId = '';
     var error = '';
-    var builder = new RequestBuilder(bootstrapcss, test, live);
     var creditCardDom;
+    var $container;
+    var jQuery = {};
+
+    var years = [];
+    var today = new Date().getFullYear();
+    for (var i = today; i <= today + 10; i++) years.push(i);
     var controls = [
-        { Name: 'Number', Lable: 'Credit Card Number' },
-        { Name: 'Name', Lable: 'Full Name' },
-        { Name: 'ExpirationMonth', Lable: 'MM' },
-        { Name: 'ExpirationYear', Lable: 'YYYY' },
-        { Name: 'CVV', Lable: 'CVV' }
+        { Name: 'Number', Lable: 'Credit Card Number', Type: "input" },
+        { Name: 'Name', Lable: 'Full Name', Type: "input" },
+        { Name: 'ExpirationMonth', Lable: 'MM', Type: "combo", Values: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'] },
+        { Name: 'ExpirationYear', Lable: 'YYYY', Type: "combo", Values: years },
+        { Name: 'CVV', Lable: 'CVV', Type: "input" }
     ];
 
-
+    var builder = new RequestBuilder(test, live, action);
     var request = builder.process();
-    var _$container;
+
     var api = {
-        jQuery: {}
-
+        setjQuery: function (jquery) {
+            jQuery = jquery;
+        }
     }
-
     api.loadScript = function (src, callback) {
         var js = document.createElement('script');
         js.src = src;
@@ -60,39 +63,49 @@ function ApiModule(bootstrapcss, test, live) {
     }
 
     api.loadForm = function () {
-        api.jQuery(document).ready(function ($) {
-            _$container = $("#" + request.element);
+        jQuery(document).ready(function ($) {
+            $container = $("#" + request.element);
 
             cssLoader.init($, request);
 
             if (isRequestValid()) {
-                addFormTo();
+                addFormElements();
                 return;
             }
-            _$container.append('<hr/><h3>CDW Form can not be used </h3><p>' + error + '</p>');
+            $container.append('<hr/><h3>CDW Form can not be used </h3><p>' + error + '</p>');
 
         });
 
     }
 
-    function addFormTo() {
+    function addFormElements() {
         formId = 'cdw_form_' + new Date().getTime();
         btnId = 'cdw_btn' + new Date().getTime();
 
-        //add form      
-        _$container.append('<hr/><h3>CDW Form:</h3><hr/>');
-        _$container.append('<form id=' + formId + '></form>');
-        var $form = $("#" + formId);
+        //add box     
+        var box = '<div class="cdw_form"></div>';
+        $container.append(box);
+        var $box = $(".cdw_form");
+        $box.append('<hr/><h3>CDW Form:</h3><hr/>');
 
-        //add conrols to form
+        //add conrols to box
         for (var i = 0; i < controls.length; i++) {
-            $form.append(domControlsLoader.inputbox(controls[i].Name, controls[i].Lable));
+            $box.append(domControlsLoader.initControl(controls[i]));
         }
-        //add button to form
-        $form.append('<button id=' + btnId + 'type= "submit" class="btn btn-primary" >Validate</button >');
-
-        $form.on('submit', submitForm);
-        $(".cdw_validations").hide();
+        //add button to box
+        if (!request.hidebutton) {
+            $box.append('<input id=' + btnId + ' type="button" class="cdw_button" value="Validate">');
+            $("#" + btnId).on('click', submitForm);
+            $('body').keyup(function (event) {
+                if (event.keyCode === 13) {
+                    submitForm();
+                }
+            });
+        }
+        else//or use partner's button
+        {
+            $("#" + request.partnerbutton).on('click', submitForm);
+        }
 
         //get all controls for later
         creditCardDom = domControlsLoader.creditCardDom(controls);
@@ -137,25 +150,38 @@ function ApiModule(bootstrapcss, test, live) {
                 pos = "Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude;
             }
         }
-        var jqxhr = $.post(request.server + "123", data, function () {
-            // alert("success");
-            var i = a;
+        $.ajax({
+            type: "POST",
+            url: request.authorize,
+            dataType: "json",
+            data: data,
+            success: function (result) {
+                sendCallBack(result.TransactionId);
 
-        })
-            .done(function (a, b, c) {
-                sendCallBack("done");
+            },
+            error: function (err) {
+                sendCallBack("error");
+                if (err.statusText === "Unauthorized") {
+                    sendCallBack("Unauthorized");
+                    return;
+                }
+                var data = err.responseJSON;
+                for (var j = 0; j < data.length; j++) {
+                    var ctr = creditCardDom[data[j]];//TODO; ned to have good fields
+                    ctr = { "Field": "Number", "Message": "Invalid Credit Card" };
+                    if ($("#cdw_" + ctr.Field).length) {
 
-            })
-            .fail(function (a, b, c) {
-                sendCallBack("failed");
-                //TODO: add validation updates here
+                        $(".cdw_val" + ctr.Field).show();
+                        $(".cdw_val" + ctr.Field).html(data[j].Message);
+                    }
+                }
 
-            })
-            .always(function () {
-               // sendCallBack("always");
-            });
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + request.token);
+            }
+        });
 
-     
 
         return false;
 
@@ -180,22 +206,35 @@ function ApiModule(bootstrapcss, test, live) {
             error = "transactionId needs to be provided as a 'data-transaction-id'";
             return false;
         }
+        if (request.hidebutton === "true") {//true
+            request.hidebutton = true;
+        }
+        else if (request.hidebutton === "false" || request.hidebutton === undefined) {//false
+            request.hidebutton = false;
+        } else {//bad data
+            request.hidebutton = false;
+        }
+        if (request.hidebutton === true && request.partnerbutton == undefined) {
+            request.hidebutton = false;
+        }
         return true;
     }
+
     function sendCallBack(msg) {
         if ($("#" + request.callbackelement).length > 0) {
             $("#" + request.callbackelement).html(msg);
         }
 
     }
+
     return api;
 
 };
 
-function RequestBuilder(bootstrapcss, test, live) {
-    var _bootstrapcss = bootstrapcss;
+function RequestBuilder(test, live, action) {
     var _test = test;
     var _live = live;
+    var _action = action;
     return {
         process: function _process() {
             var request = {};
@@ -206,13 +245,17 @@ function RequestBuilder(bootstrapcss, test, live) {
             request.element = scriptTag.getAttribute('data-element');
             request.callbackelement = scriptTag.getAttribute('data-callback-element');
             request.transactionId = scriptTag.getAttribute('data-transaction-id');
+            request.hidebutton = scriptTag.getAttribute('data-no-button');
+            request.partnerbutton = scriptTag.getAttribute('data-my-button');
 
             request.server = _test; //default
 
             if (request.reach === 'pk_live') {
                 request.server = _live;
             }
-            request.bostrapcss = _bootstrapcss;
+            request.sitecss = request.server + "/css/site.css";
+            request.authorize = request.server + _action;
+
             return request;
 
         }
@@ -223,19 +266,19 @@ var cssLoader = (function () {
 
     return {
         init: function ($, request) {
-            $("<link>",
-                {
-                    rel: "stylesheet",
-                    type: "text/css",
-                    href: request.bostrapcss
-                }).appendTo('head');
-
             //$("<link>",
             //    {
             //        rel: "stylesheet",
             //        type: "text/css",
-            //        href: request.sitecss
+            //        href: request.bostrapcss
             //    }).appendTo('head');
+
+            $("<link>",
+                {
+                    rel: "stylesheet",
+                    type: "text/css",
+                    href: request.sitecss
+                }).appendTo('head');
 
         }
     }
@@ -252,37 +295,35 @@ var domValidator = (function () {
             var mm = $("#cdw_ExpirationMonth").val();
             var yy = $("#cdw_ExpirationYear").val();
             var cvv = $("#cdw_CVV").val();
-            var errorClass = "has-error has-feedback";
-            $(".ccNumber").removeClass(errorClass);
-            $(".cdw_validations").hide();
+
+            $(".cdw_error").hide();
 
             if (ccNumber.length < 12) {
-                $(".ccNumber").addClass(errorClass);
-                $(".ccNumber").show();
+                $(".cdw_valNumber").html("Number is required > 12");
+
+                $(".cdw_valNumber").show();
                 isvalid = false;
             }
             if (name.length < 3) {
-                $(".ccName").addClass(errorClass);
-                $(".ccName").show();
-
+                $(".cdw_valName").html("Name is required");
+                $(".cdw_valName").show();
                 isvalid = false;
             }
             if (mm.length < 1 || mm < 1 || mm > 12) {
-                $(".ccExpirationMonth").addClass(errorClass);
-                $(".ccExpirationMonth").show();
-
+                $(".cdw_valExpirationMonth").html("MM is required");
+                $(".cdw_valExpirationMonth").show();
                 isvalid = false;
             }
             if (yy.length < 4 || yy < new Date().getYear()) {
-                $(".ccExpirationYear").addClass(errorClass);
-                $(".ccExpirationYear").show();
+                $(".cdw_valExpirationYear").html("YYYY is required");
+                $(".cdw_valExpirationYear").show();
 
                 isvalid = false;
             }
             if (cvv.length < 1 || cvv < 1) {
-                $(".ccCVV").addClass(errorClass);
-                $(".ccCVV").show();
+                $(".cdw_valCVV").html("CVV is required");
 
+                $(".cdw_valCVV").show();
                 isvalid = false;
             }
 
@@ -296,7 +337,7 @@ var domValidator = (function () {
 var domControlsLoader = (function () {
 
     return {
-        inputbox: initInputBox,
+        initControl: initControl,
         creditCardDom: creditCardDom
 
     }
@@ -308,17 +349,46 @@ var domControlsLoader = (function () {
         }
         return obj;
     }
+    function initControl(control) {
+        if (control.Type === 'combo') {
+            //  [i].Name, controls[i].Lable//
+            return initComboBox(control.Name, control.Lable, control.Values);
+
+        }
+        if (control.Type === 'input') {
+            return initInputBox(control.Name, control.Lable);
+        }
+    }
+
+    function initComboBox(controlName, label, values) {
+        var id = "cdw_" + controlName;
+        var className = "cdw_cc" + controlName;
+        var options = "<option> </option>";
+        for (var i = 0; i < values.length; i++) {
+            options = options + ' <option value="' + values[i] + '">' + values[i] + '</option>';
+        }
+        var html =
+            '<div class="form-group ' +
+            className +
+            '">' +
+            '<label class="cdw_label" for="' + id + '">' + label + ':</label>' +
+            '<select id=' + id + ' class=cdw_select >' + options + '</select>' +
+            '<span class=" cdw_error cdw_padd cdw_val' + controlName + '">*</span>' +
+            '</div>';
+        return html;
+    }
+
     function initInputBox(controlName, label) {
         var id = "cdw_" + controlName;
-        var className = "cc" + controlName;
+        var className = "cdw_cc" + controlName;
 
         var html =
             '<div class="form-group ' +
             className +
             '">' +
-            '<label for="' + id + '">' + label + ':</label>' +
-            '<input type="text" class="form-control" id="' + id + '" placeholder="' + label + '" required/> ' +
-            '<div class="glyphicon glyphicon-remove form-control-feedback ' + className + ' cdw_validations"></div> ' +
+            '<label class="cdw_label" for="' + id + '">' + label + ':</label>' +
+            '<input type="text" class="cdw_input" id="' + id + '" placeholder="' + label + '" required/> ' +
+            '<span class="cdw_error cdw_val' + controlName + '">*</span>' +
             '</div>';
         return html;
 
